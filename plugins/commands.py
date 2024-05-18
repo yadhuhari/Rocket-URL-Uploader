ba9d51bbe69709bbdb6ccc501a1b69be4d247aed
GOD OF THUNDER <93386525+yadhuhari@users.noreply.github.com>
2024-05-18 14:17:46 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -17,11 +17,9 @@ PICS = [
 ]
 
 
-@Client.on_message(
-    filters.command("start") & filters.private,
-)
+@Client.on_message(filters.command("start") & filters.private)
 async def start_bot(_bot, m: Message):
-    return await m.reply_photo(
+    await m.reply_photo(
         photo=random.choice(PICS),
         Translation.START_TEXT.format(m.from_user.first_name),
         reply_markup=Translation.START_BUTTONS,
@@ -29,11 +27,9 @@ async def start_bot(_bot, m: Message):
     )
 
 
-@Client.on_message(
-    filters.command("help") & filters.private,
-)
+@Client.on_message(filters.command("help") & filters.private)
 async def help_bot(_bot, m: Message):
-    return await m.reply_photo(
+    await m.reply_photo(
         photo=random.choice(PICS),
         Translation.HELP_TEXT,
         reply_markup=Translation.HELP_BUTTONS,
@@ -41,13 +37,10 @@ async def help_bot(_bot, m: Message):
     )
 
 
-@Client.on_message(
-    filters.command("about") & filters.private,
-)
+@Client.on_message(filters.command("about") & filters.private)
 async def aboutme(_bot, m: Message):
-    return await m.reply_photo(
+    await m.reply_photo(
         photo=random.choice(PICS),
         Translation.ABOUT_TEXT,
-        reply_markup=Translation.ABOUT_BUTTONS,
-        disable_web_page_preview=True,
+        reply_markup=Translation.ABOUT_BUTTONS
     )
